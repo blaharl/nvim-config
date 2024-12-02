e0c954020c38130ce8f478a09d935e57efb7900a
pointing <pointing@disroot.org>
2024-12-02 22:13:37 +0900

    added debugging configs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,68 +1,89 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require("configs.treesitter")
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("configs.treesitter")
+		end,
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require ("nvchad.configs.lspconfig").defaults()
-      require ("configs.lspconfig")
-    end,
-  },
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("nvchad.configs.lspconfig").defaults()
+			require("configs.lspconfig")
+		end,
+	},
 
-  {
-      "williamboman/mason-lspconfig.nvim",
-      event = "VeryLazy",
-      dependencies = { "nvim-lspconfig" },
-      config = function()
-          require("configs.mason-lspconfig")
-      end,
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = "VeryLazy",
+		dependencies = { "nvim-lspconfig" },
+		config = function()
+			require("configs.mason-lspconfig")
+		end,
+	},
 
-  {
-      "mfussenegger/nvim-lint",
-      event = { "BufReadPre", "BufNewFile" },
-      config = function()
-          require("configs.lint")
-      end,
-  },
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("configs.lint")
+		end,
+	},
 
-  {
-      "rshkarin/mason-nvim-lint",
-      event = "VeryLazy",
-      dependencies = { "nvim-lint" },
-      config = function()
-          require("configs.mason-lint")
-      end,
-  },
+	{
+		"rshkarin/mason-nvim-lint",
+		event = "VeryLazy",
+		dependencies = { "nvim-lint" },
+		config = function()
+			require("configs.mason-lint")
+		end,
+	},
 
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    config = function()
-      require "configs.conform"
-    end,
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre",
+		config = function()
+			require("configs.conform")
+		end,
+	},
 
-  {
-      "zapling/mason-conform.nvim",
-      event = "VeryLazy",
-      dependencies = { "conform.nvim" },
-      config = function()
-          require("configs.mason-conform")
-      end,
-  },
+	{
+		"zapling/mason-conform.nvim",
+		event = "VeryLazy",
+		dependencies = { "conform.nvim" },
+		config = function()
+			require("configs.mason-conform")
+		end,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      git = { enable = true },
-    },
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			git = { enable = true },
+		},
+	},
+
+	{
+		"mfussenegger/nvim-dap",
+		-- NOTE: And you can specify dependencies as well
+		dependencies = {
+			-- Creates a beautiful debugger UI
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+
+			-- Installs the debug adapters for you
+			"williamboman/mason.nvim",
+			"jay-babu/mason-nvim-dap.nvim",
+
+			-- Add your own debuggers here
+			"leoluz/nvim-dap-go",
+		},
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("configs.debug")
+		end,
+	},
 }
