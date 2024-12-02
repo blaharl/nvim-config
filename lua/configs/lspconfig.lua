1f347c11857e2d2c219d5c2177a8262c62a83c26
pointing <pointing@disroot.org>
2024-12-03 02:25:52 +0900

    added rustaceanvim

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,5 +1,5 @@
-local nvlsp = require "nvchad.configs.lspconfig"
-local lspconfig = require "lspconfig"
+local nvlsp = require("nvchad.configs.lspconfig")
+local lspconfig = require("lspconfig")
 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
@@ -8,66 +8,65 @@ local capabilities = require("nvchad.configs.lspconfig").capabilities
 nvlsp.defaults() -- loads nvchad's defaults
 
 lspconfig.servers = {
-  "lua_ls",
+	"lua_ls",
 }
 
 local default_servers = {
-  "clangd",
-  "rust_analyzer",
-  "nil_ls",
+	"clangd",
+	-- "rust_analyzer",
+	"nil_ls",
 }
 
 -- lsps with default config
 for _, lsp in ipairs(default_servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = nvlsp.on_attach,
+		on_init = nvlsp.on_init,
+		capabilities = nvlsp.capabilities,
+	})
 end
 
--- Without the loop, you would have to manually set up each LSP 
--- 
+-- Without the loop, you would have to manually set up each LSP
+--
 -- lspconfig.html.setup {
 --   on_attach = nvlsp.on_attach,
 --   capabilities = nvlsp.capabilities,
 -- }
 
 lspconfig.lua_ls.setup({
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+	on_attach = on_attach,
+	on_init = on_init,
+	capabilities = capabilities,
 
-  settings = {
-    Lua = {
-      diagnostics = {
-        enable = false, -- Disable all diagnostics from lua_ls
-        -- globals = { "vim" },
-      },
-      workspace = {
-        library = {
-            vim.fn.expand("$VIMRUNTIME/lua"),
-            vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
-            vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
-            vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
-            "${3rd}/love2d/library",
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			diagnostics = {
+				enable = false, -- Disable all diagnostics from lua_ls
+				-- globals = { "vim" },
+			},
+			workspace = {
+				library = {
+					vim.fn.expand("$VIMRUNTIME/lua"),
+					vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+					vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+					vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+					"${3rd}/love2d/library",
+				},
+				maxPreload = 100000,
+				preloadFileSize = 10000,
+			},
+		},
+	},
 })
 
 lspconfig.clangd.setup({
-    on_attach = function(client, bufnr)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-        on_attach(client, bufnr)
-    end,
-    on_init = on_init,
-    capabilities = capabilities,
+	on_attach = function(client, bufnr)
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+		on_attach(client, bufnr)
+	end,
+	on_init = on_init,
+	capabilities = capabilities,
 
-  settings = {
-  },
+	settings = {},
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -86,4 +86,14 @@ return {
 			require("configs.debug")
 		end,
 	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5", -- Recommended
+		lazy = false, -- This plugin is already lazy
+		["rust-analyzer"] = {
+			cargo = {
+				allFeatures = true,
+			},
+		},
+	},
 }
