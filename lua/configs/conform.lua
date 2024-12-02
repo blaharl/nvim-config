aceb034b6018ce211b62c777d62ed51f63394145
pointing <pointing@disroot.org>
2024-12-02 17:30:43 +0900

    restructured configs

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,8 +1,28 @@
 local options = {
-  lsp_fallback = true,
-
   formatters_by_ft = {
     lua = { "stylua" },
+    c = { "clang-format" },
+    cpp = { "clang-format" },
+  },
+
+  formatters = {
+    ["clang-format"] = {
+        -- IndentWidth: 4, \
+        -- TabWidth: 4, \
+      prepend_args = {
+        "-style={ \
+        UseTab: Never, \
+        AccessModifierOffset: 0, \
+        IndentAccessModifiers: true, \
+        PackConstructorInitializers: Never}",
+      },
+    },
+  },
+
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
   },
 }
 
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,12 +1,24 @@
 local nvlsp = require "nvchad.configs.lspconfig"
 local lspconfig = require "lspconfig"
 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
 nvlsp.defaults() -- loads nvchad's defaults
 
-local servers = { "clangd", "rust_analyzer", "nil_ls" }
+lspconfig.servers = {
+  "lua_ls",
+}
+
+local default_servers = {
+  "clangd",
+  "rust_analyzer",
+  "nil_ls",
+}
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
+for _, lsp in ipairs(default_servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
@@ -20,3 +32,42 @@ end
 --   on_attach = nvlsp.on_attach,
 --   capabilities = nvlsp.capabilities,
 -- }
+
+lspconfig.lua_ls.setup({
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+
+  settings = {
+    Lua = {
+      diagnostics = {
+        enable = false, -- Disable all diagnostics from lua_ls
+        -- globals = { "vim" },
+      },
+      workspace = {
+        library = {
+            vim.fn.expand("$VIMRUNTIME/lua"),
+            vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+            vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+            vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+            "${3rd}/love2d/library",
+        },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
+    },
+  },
+})
+
+lspconfig.clangd.setup({
+    on_attach = function(client, bufnr)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+        on_attach(client, bufnr)
+    end,
+    on_init = on_init,
+    capabilities = capabilities,
+
+  settings = {
+  },
+})
