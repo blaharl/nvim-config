b09e617a777673f8b4e61d428c8e65c11fe67567
pointing <pointing@disroot.org>
2025-01-19 01:36:59 +0900

    added python and prettier formatters

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,29 +1,50 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    c = { "clang-format" },
-    cpp = { "clang-format" },
-  },
+	ensure_installed = {
+		"stylua",
+		"prettier",
+		"isort",
+		"black",
+		"clang-format",
+	},
 
-  formatters = {
-    ["clang-format"] = {
-        -- IndentWidth: 4, \
-        -- TabWidth: 4, \
-      prepend_args = {
-        "-style={ \
-        UseTab: Never, \
-        AccessModifierOffset: 0, \
-        IndentAccessModifiers: true, \
-        PackConstructorInitializers: Never}",
-      },
-    },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		svelte = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		graphql = { "prettier" },
+		-- python = { "isort", "black" },
+		c = { "clang-format" },
+		cpp = { "clang-format" },
+	},
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	formatters = {
+		-- ["clang-format"] = {
+		-- 	-- IndentWidth: 4, \
+		-- 	-- TabWidth: 4, \
+		-- 	prepend_args = {
+		-- 		"-style={ \
+		--       UseTab: Never, \
+		--       AccessModifierOffset: 0, \
+		--       IndentAccessModifiers: true, \
+		--       PackConstructorInitializers: Never}",
+		-- 	},
+		-- },
+	},
+
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		-- async = false,
+		lsp_fallback = true,
+	},
 }
 
 require("conform").setup(options)
