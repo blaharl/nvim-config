5480fe082979afb562bcaa6eeb7db5cebf14e7f6
pointing <pointing@disroot.org>
2025-01-19 01:28:26 +0900

    disabled treesitter indenting(collision w/ clangd)

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,27 +1,27 @@
 local options = {
-    ensure_installed = {
-        "bash",
-        "c",
-        "cmake",
-        "cpp",
-        "fish",
-        "lua",
-        "luadoc",
-        "make",
-        "markdown",
-        "printf",
-        "toml",
-        "vim",
-        "vimdoc",
-        "yaml",
-    },
+	ensure_installed = {
+		"bash",
+		"c",
+		"cmake",
+		"cpp",
+		"fish",
+		"lua",
+		"luadoc",
+		"make",
+		"markdown",
+		"printf",
+		"toml",
+		"vim",
+		"vimdoc",
+		"yaml",
+	},
 
-    highlight = {
-        enable = true,
-        use_languagetree = true,
-    },
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
 
-    indent = { enable = true },
+	indent = { enable = false },
 }
 
 require("nvim-treesitter.configs").setup(options)
