5291dcdd0dc8aa4a4552d4df96910a7ee13e92a8
pointing <pointing@disroot.org>
2025-02-08 22:18:50 +0900

    fixed -cro options not working for certain ft

--- lua/options.lua
+++ lua/options.lua
@@ -8,8 +8,10 @@ local g = vim.g
 
 o.relativenumber = true
 
-vim.cmd("autocmd BufEnter * set formatoptions-=cro")
-vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro")
+vim.cmd([[
+autocmd Filetype * set formatoptions-=cro
+autocmd Filetype * setlocal formatoptions-=cro
+]])
 
 vim.opt.langmap =
 	"ㅁa,ㅠb,ㅊc,ㅇd,ㄷe,ㄹf,ㅎg,ㅗh,ㅑi,ㅓj,ㅏk,ㅣl,ㅡm,ㅜn,ㅐo,ㅔp,ㅂq,ㄱr,ㄴs,ㅅt,ㅕu,ㅍv,ㅈw,ㅌx,ㅛy,ㅋz"
