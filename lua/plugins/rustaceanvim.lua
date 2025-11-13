be107b285b8020f50ad81926648a92e3fd3d99c8
pointing <pointing@disroot.org>
2025-11-14 06:25:40 +0900

    added rustaceanvim keymaps

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -5,6 +5,14 @@ return {
 		version = "^6",
 		-- This plugin is already lazy
 		lazy = false,
+		vim.keymap.set("n", "<leader>ca", function()
+			vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+			-- or vim.lsp.buf.codeAction() if you don't want grouping.
+		end, { desc = "LSP Code Action" }),
+		vim.keymap.set("n", "<leader>ra", vim.lsp.buf.rename, { desc = "LSP Renamer" }),
+		vim.keymap.set("n", "K", function()
+			vim.cmd.RustLsp("hover", "actions")
+		end, { desc = "LSP Hover Actions" }),
 		init = function()
 			vim.g.rustaceanvim = {
 				server = {
