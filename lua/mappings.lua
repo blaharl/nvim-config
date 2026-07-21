66dce8509295afba83bd9e276f5b5615772e0422
pointing <pointing@disroot.org>
2026-07-21 18:16:37 +0900

    added diagnostic float keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -29,6 +29,7 @@ vim.diagnostic.config {
   },
 }
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'open diagnostic' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
